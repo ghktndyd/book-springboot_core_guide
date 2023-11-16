28c0ee0ae9ac36a15f5e28d057d2efe644c05812
sooyong <hwasuyong99@naver.com>
2023-11-16 17:39:43 +0900

    예제 6.23 saveProduct() 메서드 구현, page 132

---
+++ jpa/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.jpa.data.repository;
+
+import com.springboot.jpa.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 제네릭 안에 들어가있는 것은 대상 엔티티와 대상 엔티티의 @Id 필드 타입이다.
+}
---
+++ jpa/src/main/java/com/springboot/jpa/data/service/impl/ProductServiceImpl.java
@@ -0,0 +1,67 @@
+package com.springboot.jpa.data.service.impl;
+
+import com.springboot.jpa.data.dao.ProductDao;
+import com.springboot.jpa.data.dto.ProductDto;
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductDao productDao;
+
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ProductResponseDto getProduct(Long number) {
+        Product product = productDao.selectProduct(number);
+
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+
+        productResponseDto.setNumber(product.getNumber());
+        productResponseDto.setName(product.getName());
+        productResponseDto.setPrice(productResponseDto.getPrice());
+        productResponseDto.setStock(productResponseDto.getStock());
+
+        return productResponseDto;
+    }
+
+    @Override
+    public ProductResponseDto saveProduct(ProductDto productDto) {
+
+        Product product = new Product();
+
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        product.setStock(productDto.getStock());
+        product.setCreatedAt(LocalDateTime.now());
+        product.setUpdatedAt(LocalDateTime.now());
+
+        Product savedProduct = productDao.insertProduct(product);
+
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+
+        productResponseDto.setNumber(savedProduct.getNumber());
+        productResponseDto.setName(savedProduct.getName());
+        productResponseDto.setPrice(savedProduct.getPrice());
+        productResponseDto.setStock(savedProduct.getStock());
+
+        return productResponseDto;
+    }
+
+    @Override
+    public ProductResponseDto changeProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+
+    }
+}

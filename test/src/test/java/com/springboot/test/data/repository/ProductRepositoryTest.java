d399507b6aa01e02cd32e946bb895f410c5cb284
sooyong <hwasuyong99@naver.com>
2023-11-17 16:18:17 +0900

    7장 마무리, 207 page 끝

---
+++ test/src/test/java/com/springboot/test/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.springboot.test.data.repository;
+
+import com.springboot.test.data.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// 속성값을 Replace.NONE으로 하면 실제 디비 사용 가능
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void save() {
+        Product product = new Product();
+        product.setName("펜");
+        product.setPrice(1000);
+        product.setStock(1000);
+
+        Product savedProduct = productRepository.save(product);
+
+        assertEquals(product.getName(), savedProduct.getName());
+        assertEquals(product.getPrice(), savedProduct.getPrice());
+        assertEquals(product.getStock(), savedProduct.getStock());
+    }
+}
--- test/src/test/java/com/springboot/test/service/impl/ProductServiceTest2.java
+++ test/src/test/java/com/springboot/test/service/impl/ProductServiceTest2.java
@@ -1,34 +1,38 @@
 package com.springboot.test.service.impl;
 
-import com.springboot.test.data.dto.ProductDto;
-import com.springboot.test.data.dto.ProductResponseDto;
-import com.springboot.test.data.entity.Product;
-import com.springboot.test.data.repository.ProductRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import java.util.Optional;
-
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
-class ProductServiceTest {
+import com.springboot.test.data.dto.ProductDto;
+import com.springboot.test.data.dto.ProductResponseDto;
+import com.springboot.test.data.entity.Product;
+import com.springboot.test.data.repository.ProductRepository;
+import com.springboot.test.service.ProductService;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-    private final ProductRepository productRepository = Mockito.mock(ProductRepository.class);
-    private ProductServiceImpl productService;
+@ExtendWith(SpringExtension.class)
+@Import({ProductServiceImpl.class})
+class ProductServiceTest2 {
 
-    @BeforeEach
-    public void setUpTest() {
-        productService = new ProductServiceImpl(productRepository);
-    }
+    @MockBean
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductService productService;
 
     @Test
-    void getProductTest() {
+    public void getProductTest() {
+        // given
         Product givenProduct = new Product();
-
         givenProduct.setNumber(123L);
         givenProduct.setName("펜");
         givenProduct.setPrice(1000);
@@ -37,8 +41,10 @@ class ProductServiceTest {
         Mockito.when(productRepository.findById(123L))
                 .thenReturn(Optional.of(givenProduct));
 
+        // when
         ProductResponseDto productResponseDto = productService.getProduct(123L);
 
+        // then
         Assertions.assertEquals(productResponseDto.getNumber(), givenProduct.getNumber());
         Assertions.assertEquals(productResponseDto.getName(), givenProduct.getName());
         Assertions.assertEquals(productResponseDto.getPrice(), givenProduct.getPrice());
@@ -49,13 +55,15 @@ class ProductServiceTest {
 
     @Test
     void saveProductTest() {
+        // given
         Mockito.when(productRepository.save(any(Product.class)))
                 .then(returnsFirstArg());
 
+        // when
         ProductResponseDto productResponseDto = productService.saveProduct(
-                new ProductDto("펜", 1000, 1234)
-        );
+                new ProductDto("펜", 1000, 1234));
 
+        // then
         Assertions.assertEquals(productResponseDto.getName(), "펜");
         Assertions.assertEquals(productResponseDto.getPrice(), 1000);
         Assertions.assertEquals(productResponseDto.getStock(), 1234);
